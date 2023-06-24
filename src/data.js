8bcb08df323995b888b56a4b85e576353d06de25
Yazid <mti.yazid@gmail.com>
2023-06-24 03:45:12 +0100

    waiting for meet

--- src/data.js
+++ src/data.js
@@ -2,60 +2,84 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
+    // {
+    //   text: 'Landing',
+    //   links: [
+    //     {
+    //       text: 'Sass',
+    //       href: getPermalink('/landing/saas'),
+    //     },
+    //     {
+    //       text: 'Startup',
+    //       href: getPermalink('/landing/startup'),
+    //     },
+    //     {
+    //       text: 'Mobile App',
+    //       href: getPermalink('/landing/mobile-app'),
+    //     },
+    //   ],
+    // },
+    // {
+    //   text: 'Pages',
+    //   links: [
+    //     {
+    //       text: 'Features',
+    //       href: '#',
+    //     },
+    //     {
+    //       text: 'Pricing',
+    //       href: '#',
+    //     },
+    //     {
+    //       text: 'About us',
+    //       href: '#',
+    //     },
+    //     {
+    //       text: 'Contact',
+    //       href: '#',
+    //     },
+    //     {
+    //       text: 'Terms',
+    //       href: getPermalink('/terms'),
+    //     },
+    //     {
+    //       text: 'Privacy policy',
+    //       href: getPermalink('/privacy'),
+    //     },
+    //   ],
+    // },
     {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Sass',
-          href: getPermalink('/landing/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/landing/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/landing/mobile-app'),
-        },
-      ],
-    },
-    {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features',
-          href: '#',
-        },
-        {
-          text: 'Pricing',
-          href: '#',
-        },
-        {
-          text: 'About us',
-          href: '#',
-        },
-        {
-          text: 'Contact',
-          href: '#',
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
+      text: 'Over ons',
       href: '#',
     },
     {
-      text: 'Blog',
-      href: getBlogPermalink(),
+      text: 'Diensten',
+      href: '#',
     },
+    {
+      text: 'Projecten',
+      href: '#',
+    },
+    {
+      text: 'Partners',
+      href: '#',
+    },
+    {
+      text: 'Werkstroom',
+      href: '#',
+    },
+    {
+      text: 'Getuigenissen',
+      href: '#',
+    },
+    // {
+    //   text: 'Widgets',
+    //   href: '#',
+    // },
+    // {
+    //   text: 'Blog',
+    //   href: getBlogPermalink(),
+    // },
   ],
   // actions: [
   //   { type: 'button', text: 'Downloassd', href: 'https://github.com/onwidget/astrowind' }
